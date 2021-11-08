94932e91092d47955d1d89a935e0cc3581a47e76
khunnatta <61150735+khunnatta@users.noreply.github.com>
2021-11-08 10:43:30 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,26 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+<div class="main">
+  <header>
+    <div class="topNav">
+      <img class="navLogo" src="../assets/logo.png" alt="">
+      <ul class="navList">
+        <li><a href="/">Home</a></li>
+        <li><a href="/">List</a></li>
+      </ul>
+    </div>
+  </header>
+
+  <div class="part1">
+    <div class="section1">
+      <h1>Section1</h1>
+    </div>
+    <div class="section2">
+      <h1>Section2</h1>
+    </div>
   </div>
+</div>
+
 </template>
 
 <script>
@@ -41,6 +34,79 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Prompt:wght@200;300;400;500;600;700&display=swap');
+
+.main {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+header {
+  width: 100%;
+  background:#9fefc8;
+}
+
+.topNav{
+  width: 90%;
+  margin: 0 auto;
+  background: #9fefc8;
+  display: flex;
+  flex-direction: row;
+  align-items:center;
+  justify-content: space-between;
+}
+
+.navLogo{
+  margin: 5px 0px;
+  width: 3rem;
+}
+
+.navList{
+  font-family: 'Prompt', sans-serif;
+  font-size: 20px;
+  margin: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+a:hover {
+  color: #e9eceb
+}
+
+.part1{
+  width: 80%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.section1{
+  width: 60%;
+  height: 500px;
+  border: 2px solid;
+  border-color: rgba(128, 128, 128, 0.541);
+  border-radius: 15px;
+  margin: 10px;
+}
+
+.section2{
+  width: 40%;
+  height: 500px;
+  border: 2px solid;
+  border-color: rgba(128, 128, 128, 0.541);
+  border-radius: 15px;
+  margin: 10px;
+}
+
+h1{
+  color: rgba(128, 128, 128, 0.75);
+}
+
 h3 {
   margin: 40px 0 0;
 }
